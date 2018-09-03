38b896bcf260d8d5f29145a5d94e14288a483f3d
AbelVasquezYuto <abelfebrero25@gmail.com>
2018-09-02 20:17:32 -0500

    nav

--- app/src/main/java/com/example/paola/splash/MainActivity.java
+++ app/src/main/java/com/example/paola/splash/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mBt_sign_in;
     private TextView mTv_registrese;
+    private EditText mEt_usuario;
+    private EditText mEt_contraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +23,20 @@ public class MainActivity extends AppCompatActivity {
 
         mBt_sign_in = (Button) findViewById(R.id.bt_sign_in);
         mTv_registrese = (TextView) findViewById(R.id.tv_registrese);
+        mEt_usuario = (EditText) findViewById(R.id.et_usuario);
+        mEt_contraseña = (EditText) findViewById(R.id.et_contraseña);
 
         mBt_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                if(mEt_usuario.getText().toString().equalsIgnoreCase("admin")){
+                    Intent intent = new Intent(MainActivity.this,MainActivity_NavigationDrawer.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Usuario incorrecto", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
